f541b8ef9ca55a251e776f165f77983c4edafbdd
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-07-04 08:59:56 +0200

    simplified editor

--- client/views/editor/editor_form.js
+++ client/views/editor/editor_form.js
@@ -32,13 +32,8 @@ var LEFT_FIELDS = {
 				type: 'button',
 				btnType: 'link',
 				events: {
-					click: function(e, dataCtxt) {
-						var self = this;
-						self.writeData($('.editor-modal'), function(data) {
-							console.log(this, arguments);
-							// data.senses.splice(dataCtxt.idx, 1);
-							// self.refresh();
-						});
+					click: function(e) {
+						$(e.currentTarget).parents('.array-element').remove();
 					}
 				}
 			}
@@ -49,23 +44,110 @@ var LEFT_FIELDS = {
 		type: 'button',
 		btnType: 'success',
 		events: {
-			click: function() {
-				var self = this;
-				self.writeData($('.editor-wrapper'), function(data) {
-					// data.senses.push({});
-					// self.refresh();
-				});
+			click: function(e) {
+				var data = {
+					fields: this.parentContext.fields[3].fields,
+					data: [{}]
+				};
+				var $nextEl = $(e.currentTarget).parent()[0];
+				var comp = UI.renderWithData(Template.appEditorArrayWrapper, data);
+				// var comp = UI.renderWithData(Template.aet, data);
+				UI.insert(comp, $('.editor-left-side .row')[0],$nextEl);
 			}
 		}
 	}
 };
+var SHORT_CLASS = 'short col-sm-3';
 
-//     'options': null,                        // object with k-v pairs or simple [] (data provider for select/radio/checkbox)
-//     'label': 'Field label',                 // defaults to capitalized field name,
-//     'default': null,                        // default value when no value object is provided
-//     'post': function (v) { return v; },     // method executed after data retrieval
-//     'render': function () { return true; }  // method executed to check if field has to be rendered
-
+var RIGHT_FIELDS = {
+	wordRight: {
+		'class': 'col-xs-12',
+		placeholder: 'cselekvés, tett'
+	},
+	alternatives: {
+		type: 'array',
+		fields: {
+			category: {
+				'class': SHORT_CLASS.replace('3', '4'),
+				placeholder: 'fiz.'
+			},
+			meaning: {
+				'class': SHORT_CLASS.replace('3', '4'),
+				placeholder: 'vegyhatás'
+			},
+			removeAlternative: {
+				type: 'button',
+				btnType: 'link',
+				// 'class': SHORT_CLASS,
+				events: {
+					click: function(e) {
+						$(e.currentTarget).parents('.array-element').remove();
+					}
+				}
+			}
+		}
+	},
+	addAlternative: {
+		'class': 'col-xs-12',
+		type: 'button',
+		btnType: 'success',
+		events: {
+			click: function(e) {
+				var data = {
+					fields: this.parentContext.fields[1].fields,
+					data: [{}]
+				};
+				var $nextEl = $(e.currentTarget).parent()[0];
+				var comp = UI.renderWithData(Template.appEditorArrayWrapper, data);
+				// var comp = UI.renderWithData(Template.aet, data);
+				UI.insert(comp, $('.editor-right-side .row')[0],$nextEl);
+			}
+		}
+	},
+	senses: {
+		type: 'array',
+		fields: {
+			phrase: {
+				'class': 'col-xs-12',
+				placeholder: 'Acţiune pauliană (acţiune revocatorie)'
+			},
+			description: {
+				'class': 'col-xs-12',
+				placeholder: 'Este acea acţiune prin care creditorul atacă actul de înstrăinare a bunului de către debitor, dacă acesta din urmă îşi are intenţia de a-l înşela sau de a ocoli pe creditor.'
+			},
+			example: {
+				'class': 'col-xs-12',
+				placeholder: '...'
+			},
+			removeSense: {
+				type: 'button',
+				btnType: 'link',
+				events: {
+					click: function(e) {
+						$(e.currentTarget).parents('.array-element').remove();
+					}
+				}
+			}
+		}
+	},
+	addSense: {
+		'class': 'col-xs-12',
+		type: 'button',
+		btnType: 'success',
+		events: {
+			click: function(e) {
+				var data = {
+					fields: this.parentContext.fields[3].fields,
+					data: [{}]
+				};
+				var $nextEl = $(e.currentTarget).parent()[0];
+				var comp = UI.renderWithData(Template.appEditorArrayWrapper, data);
+				// var comp = UI.renderWithData(Template.aet, data);
+				UI.insert(comp, $('.editor-left-side .row')[0],$nextEl);
+			}
+		}
+	}
+};
 
 
 var buildLeftFormData = function(data) {
@@ -78,10 +160,12 @@ var buildLeftFormData = function(data) {
 };
 
 var buildRightFormData = function(data) {
-	// buildLeftFormData(data);
-	data = 3;
-	return {};
-
+	var fields = _.extend({}, RIGHT_FIELDS);
+	fields = App.editor.buildFieldArr(fields, data);
+	return {
+		fields: fields,
+		data: data
+	};
 };
 
 App.editor.buildFormData = function(data) {
