a59057624baa0f6e9ea68f226fa7b1727eb6a4c3
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-07-05 01:58:16 +0200

    corrected editor

--- client/views/editor/editor.js
+++ client/views/editor/editor.js
@@ -10,12 +10,51 @@ var clean = function(str) {
 	return cstr;
 };
 
+var editorData = {
+	// itemId: {
+	// fields: fields,
+	// data: data
+	// }
+};
+
+var extractSearchableStr = function (data) {
+	var SKIP = ['endingLeft', 'typeLeft', 'category'];
+	var str = [];
+	var f = function (obj) {
+		_.each(obj, function (v, k) {
+			if (_.isString(v) && SKIP.indexOf(k) === -1) {
+				str.push(v);
+			} else if (_.isArray(v)) {
+				_.each(v, f);
+			} else if (_.isObject(v)) {
+				f(v);
+			}
+		});
+	};
+	f(data);
+	return clean(str.join(' '));
+};
+
 Template.editor.events({
 	'click .save.btn': function() {
-		throw 'NE';
-		// closeEditor();
+		var id = Session.get('editor.itemId');
+		var ed = editorData[id];
+		var ldata = App.editor.collectData(ed.fields.leftWordFormData.fields, $('.editor-left-word'));
+		var rdata = App.editor.collectData(ed.fields.rightWordFormData.fields, $('.editor-right-word'));
+		var sensesData = App.editor.collectData(ed.fields.sensesFormData.fields, $('.editor-senses'));
+		var data = _.extend({}, ldata, rdata, sensesData);
+		delete editorData[id];
+		data.searchable = extractSearchableStr(data);
+		if (id === -1) {
+			ItemsCollection.insert(data);
+		} else {
+			ItemsCollection.update(id, {$set: data});
+		}
+		closeEditor();
 	},
 	'click .cancel.btn': function() {
+		var id = Session.get('editor.itemId');
+		delete editorData[id];
 		closeEditor();
 	}
 });
@@ -24,19 +63,12 @@ Template.editor.helpers({
 	data: function() {
 		var id = Session.get('editor.itemId');
 		var data = id === -1 ? {} : ItemsCollection.findOne(id);
-		data = {
-			senses: [{
-				phrase: 'phrase',
-				description: 'description',
-				example: 'example'
-			},{
-				phrase: 'phrase2',
-				description: 'description2',
-				example: 'example2'
-			}]
-		};
 		if (data) {
 			var fd = App.editor.buildFormData(data);
+			editorData[id] = {
+				fields: fd,
+				data: data
+			};
 			return fd;
 		}
 		return null;
@@ -49,6 +81,7 @@ var edit = function(id) {
 };
 
 App.component('editor').expose({
+	// override defult
 	edit: function(item) {
 		edit(item._id);
 	},
--- client/views/editor/editor_form.js
+++ client/views/editor/editor_form.js
@@ -13,49 +13,6 @@ var LEFT_FIELDS = {
 		'class': SHORT_CLASS,
 		placeholder: 's fn'
 	},
-	senses: {
-		type: 'array',
-		fields: {
-			phrase: {
-				'class': 'col-xs-12',
-				placeholder: 'Acţiune pauliană (acţiune revocatorie)'
-			},
-			description: {
-				'class': 'col-xs-12',
-				placeholder: 'Este acea acţiune prin care creditorul atacă actul de înstrăinare a bunului de către debitor, dacă acesta din urmă îşi are intenţia de a-l înşela sau de a ocoli pe creditor.'
-			},
-			example: {
-				'class': 'col-xs-12',
-				placeholder: '...'
-			},
-			removeSense: {
-				type: 'button',
-				btnType: 'link',
-				events: {
-					click: function(e) {
-						$(e.currentTarget).parents('.array-element').remove();
-					}
-				}
-			}
-		}
-	},
-	addSense: {
-		'class': 'col-xs-12',
-		type: 'button',
-		btnType: 'success',
-		events: {
-			click: function(e) {
-				var data = {
-					fields: this.parentContext.fields[3].fields,
-					data: [{}]
-				};
-				var $nextEl = $(e.currentTarget).parent()[0];
-				var comp = UI.renderWithData(Template.appEditorArrayWrapper, data);
-				// var comp = UI.renderWithData(Template.aet, data);
-				UI.insert(comp, $('.editor-left-side .row')[0],$nextEl);
-			}
-		}
-	}
 };
 var SHORT_CLASS = 'short col-sm-3';
 
@@ -92,38 +49,51 @@ var RIGHT_FIELDS = {
 		type: 'button',
 		btnType: 'success',
 		events: {
-			click: function(e) {
+			click: function() {
 				var data = {
 					fields: this.parentContext.fields[1].fields,
 					data: [{}]
 				};
-				var $nextEl = $(e.currentTarget).parent()[0];
 				var comp = UI.renderWithData(Template.appEditorArrayWrapper, data);
-				// var comp = UI.renderWithData(Template.aet, data);
-				UI.insert(comp, $('.editor-right-side .row')[0],$nextEl);
+				UI.insert(comp, $('.array-wrapper.alternatives')[0]);
 			}
 		}
-	},
+	}
+};
+
+var SENSES_FIELDS = {
 	senses: {
 		type: 'array',
 		fields: {
-			phrase: {
-				'class': 'col-xs-12',
+			phraseLeft: {
+				'class': 'col-xs-12 col-sm-6',
 				placeholder: 'Acţiune pauliană (acţiune revocatorie)'
 			},
-			description: {
-				'class': 'col-xs-12',
+			phraseRight: {
+				'class': 'col-xs-12 col-sm-6',
+				placeholder: 'Acţiune pauliană (acţiune revocatorie)'
+			},
+			descriptionLeft: {
+				'class': 'col-xs-12 col-sm-6',
 				placeholder: 'Este acea acţiune prin care creditorul atacă actul de înstrăinare a bunului de către debitor, dacă acesta din urmă îşi are intenţia de a-l înşela sau de a ocoli pe creditor.'
 			},
-			example: {
-				'class': 'col-xs-12',
+			descriptionRight: {
+				'class': 'col-xs-12 col-sm-6',
+				placeholder: 'Este acea acţiune prin care creditorul atacă actul de înstrăinare a bunului de către debitor, dacă acesta din urmă îşi are intenţia de a-l înşela sau de a ocoli pe creditor.'
+			},
+			exampleLeft: {
+				'class': 'col-xs-12 col-sm-6',
+				placeholder: '...'
+			},
+			exampleRight: {
+				'class': 'col-xs-12 col-sm-6',
 				placeholder: '...'
 			},
 			removeSense: {
 				type: 'button',
 				btnType: 'link',
 				events: {
-					click: function(e) {
+					click: function() {
 						$(e.currentTarget).parents('.array-element').remove();
 					}
 				}
@@ -137,30 +107,19 @@ var RIGHT_FIELDS = {
 		events: {
 			click: function(e) {
 				var data = {
-					fields: this.parentContext.fields[3].fields,
+					fields: this.parentContext.fields[0].fields,
 					data: [{}]
 				};
-				var $nextEl = $(e.currentTarget).parent()[0];
 				var comp = UI.renderWithData(Template.appEditorArrayWrapper, data);
-				// var comp = UI.renderWithData(Template.aet, data);
-				UI.insert(comp, $('.editor-left-side .row')[0],$nextEl);
+				UI.insert(comp, $('.array-wrapper.senses')[0]);
 			}
 		}
 	}
 };
 
 
-var buildLeftFormData = function(data) {
-	var fields = _.extend({}, LEFT_FIELDS);
-	fields = App.editor.buildFieldArr(fields, data);
-	return {
-		fields: fields,
-		data: data
-	};
-};
-
-var buildRightFormData = function(data) {
-	var fields = _.extend({}, RIGHT_FIELDS);
+var buildFormData = function(FIELDS, data) {
+	var fields = _.extend({}, FIELDS);
 	fields = App.editor.buildFieldArr(fields, data);
 	return {
 		fields: fields,
@@ -170,8 +129,9 @@ var buildRightFormData = function(data) {
 
 App.editor.buildFormData = function(data) {
 	var formData = {
-		leftFormOptions: buildLeftFormData(data),
-		rightFormOptions: buildRightFormData(data)
+		leftWordFormData: buildFormData(LEFT_FIELDS, data),
+		rightWordFormData: buildFormData(RIGHT_FIELDS, data),
+		sensesFormData: buildFormData(SENSES_FIELDS, data)
 	};
 	return formData;
 };
