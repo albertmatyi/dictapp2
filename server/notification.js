ced7fe9f762fd960e672a361366391755e33821f
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-12-28 16:23:05 +0200

    signout hides sign in controls always remove login button label

--- server/notification.js
+++ server/notification.js
@@ -1,4 +1,5 @@
 var APP_URL = 'lex.sapientia.ro';
+
 var options = {
   apiKey: 'key-dda9b756ccbbf539f75c0c1f1b71beaf',
   domain: 'sandbox3f5e890f18424d80a3e812f035848ced.mailgun.org'
@@ -18,7 +19,7 @@ Meteor.methods({
     console.log('requested definition for:', word);
     NigerianPrinceGun.send({
      'to': App.property('notifications.destinationEmail'),
-     'from': 'no-reply@dictapp.com',
+     'from': 'no-reply@lex.sapientia.ro',
      'html': '<html><head></head><body>The word: ":word" has been requested. <br/> Please go to <a href="http://'+APP_URL+'/search/:word">dictapp</a> and define it</body></html>'.replace(/:word/g, word),
      'text': 'The word: ":word" has been requested. \n Please go to http://'+APP_URL+'/search/:word and define it'.replace(/:word/g, word),
      'subject': 'The word: "'+ word + '" has been requested',
